14520b7191590d311556cdfa0e9a82a1f56aed06
ivan1-git <78927667+ivan1-git@users.noreply.github.com>
2021-02-17 20:55:15 -0600

    Add files via upload

---
+++ Practica git/src/Package1/Profesores.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Profesores extends Usuario {
+
+    /**
+     * Default constructor
+     */
+    public Profesores() {
+    }
+
+    /**
+     * 
+     */
+    private string NumEmpleado;
+
+    /**
+     * 
+     */
+    private string correoElectronico1;
+
+    private static class string {
+
+        public string() {
+        }
+    }
+
+}
---
+++ Practica git/src/Package1/Usuario.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Usuario {
+
+    /**
+     * Default constructor
+     */
+    public Usuario() {
+    }
+
+    /**
+     * 
+     */
+    private string user;
+
+    /**
+     * 
+     */
+    private string psw;
+
+  
+
+    /**
+     * 
+     */
+    private string correoElectronico1;
+      private static class string {
+
+        public string() {
+        }
+    }
+
+}
