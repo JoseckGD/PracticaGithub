97fb3e23956672b036e9c4b984245af9d302414d
OTLI <ozhm@micorreo.upp.edu>
2021-02-17 21:43:50 -0600

    LISTO =D

---
+++ Desktop/OTLI/PROCESO DE DESARROLLO/ProgramaJava_otli/src/Package otli/ServicioMedico.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class ServicioMedico extends Usuario {
+
+    /**
+     * Default constructor
+     */
+    public ServicioMedico() {
+    }
+
+    /**
+     * 
+     */
+    private Void nombre;
+
+    /**
+     * 
+     */
+    private Void apellidos;
+
+    /**
+     * 
+     */
+    private Void tel;
+
+}
