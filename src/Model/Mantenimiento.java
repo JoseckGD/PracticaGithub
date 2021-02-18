5d83c9de9357c1b08333fb3390c2f711f88e5175
Crespo <crespo03@micorreo.upp.edu.mx>
2021-02-18 16:24:48 -0600

    chems_Modifique la clase mantenimiento

--- src/Model/Mantenimiento.java
+++ src/Model/Mantenimiento.java
@@ -13,4 +13,34 @@ public class Mantenimiento extends Usuario {
     public Mantenimiento() {
     }
 
+    /**
+     * 
+     */
+    public String Horario de trabajo;
+
+    /**
+     * 
+     */
+    public String Nombre del epleado;
+
+    /**
+     * 
+     */
+    public String Numero de seguro;
+
+    /**
+     * 
+     */
+    public String Sueldo;
+
+    /**
+     * 
+     */
+    public String Area donde labora;
+
+    /**
+     * 
+     */
+    public String Numero de horas extras;
+
 }
