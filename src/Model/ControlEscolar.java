0b924f1b932a9abe6c00c19ffa058f121f4cfd59
José Ensastiga <jemsastiga@gmail.com>
2021-02-19 12:06:35 -0600

    Hola Xd, 19 de Febrero del 2021, modifique la clase Control Escolar

--- src/Model/ControlEscolar.java
+++ src/Model/ControlEscolar.java
@@ -12,5 +12,16 @@ public class ControlEscolar extends Usuario {
      */
     public ControlEscolar() {
     }
+    
+    public void agregarAlumno() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void modificarAlumno() {
+        // TODO implement here
+    }
 
 }
